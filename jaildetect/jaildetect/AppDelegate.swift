d52271dc335695fa9690925f54050a15b94d310b
Julia Potapenko <julepka@gmail.com>
2020-01-04 15:08:04 +0200

    Created an Xcode project
    Commited missing file
    
    fixed untracked files

---
+++ jaildetect/jaildetect/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  jaildetect
+//
+//  Created by Julia Potapenko on 1/4/20.
+//  Copyright © 2020 Julia Potapenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ jaildetect/jaildetect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jaildetect
+//
+//  Created by Julia Potapenko on 1/4/20.
+//  Copyright © 2020 Julia Potapenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
